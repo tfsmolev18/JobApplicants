6068bfe18864e1e2463b4ccc78086c73f9a98bb1
tfsmolev18 <tfsmolev18@codingburgas.bg>
2021-07-12 10:18:43 +0300

    created add applicant function for admins

--- JobApplicant/JobApplicant.cpp
+++ JobApplicant/JobApplicant.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <fstream>
 using namespace std;
 
@@ -36,6 +37,36 @@ bool isLoggedIn()
 	}
 }
 
+void addApplicant()
+{
+	string ID;
+	cout << "Enter applicant's ID" << endl;
+	getline(cin, ID);
+	string username;
+	cout << "Enter applicant's username" << endl;
+	getline(cin, username);
+	string firstName;
+	cout << "Enter applicant's first name" << endl;
+	getline(cin, firstName);
+	string lastName;
+	cout << "Enter applicant's last name" << endl;
+	getline(cin, lastName);
+	string address;
+	cout << "Enter applicant's address" << endl;
+	getline(cin, address);
+	string education;
+	cout << "Enter applicant's level of education" << endl;
+	getline(cin, education);
+	string yearOfExperience;
+	cout << "Enter applicant's years of experience" << endl;
+	getline(cin, yearOfExperience);
+
+	ofstream file;
+	file.open(ID + " " + username + " "  + firstName + " " + lastName + " " + address + " " + education + " " + yearOfExperience + ".txt");
+	file << ID << endl << username << endl << firstName << endl << lastName << endl << address << endl << education << endl << yearOfExperience;
+	file.close();
+}
+
 int adminChoice()
 {
 	int choice;
@@ -43,21 +74,21 @@ int adminChoice()
 	cin >> choice;
 	if (choice == 1)
 	{
-		
+		addApplicant();
 	}
 	else if (choice == 2)
 	{
-		
+		return 3;
 	}
 	else if (choice == 3)
 	{
-
+		return 3;
 	}
 }
 
 int main()
 {
-	int choice;
+	/*int choice;
 	cout << "Enter 1 if you want to register or 2 if you want to login" << endl;
 	cin >> choice;
 	if (choice == 1)
@@ -91,6 +122,14 @@ int main()
 			system("PAUSE");
 			return 1;
 		}
-	}
-
+	}*/
+	/*int adminMenuChoice;
+	cout << "Enter 1 if you want to add an applicant, 2 if you want to edit applicant's information or 3 if you want to delete one" << endl;
+	cin >> adminMenuChoice;
+	system("PAUSE");
+	if (adminMenuChoice == 1)
+	{
+		
+	}*/
+	cout << adminChoice() << endl;
 }
